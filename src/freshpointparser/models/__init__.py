d0759f9337aef2125df6bcdd7218c39481c888a4
Konstantin Mykhailov <constantinemykhailov@gmail.com>
2026-01-03 13:48:18 +0100

    Merge BaseRecord and BasePage models.

--- src/freshpointparser/models/__init__.py
+++ src/freshpointparser/models/__init__.py
@@ -3,14 +3,13 @@ The ``annotations`` submodule is available for additional data types.
 """
 
 from . import types
-from ._base import BaseItem, BasePage, BaseRecord, logger
+from ._base import BaseItem, BasePage, logger
 from ._location import Location, LocationPage, get_location_page_url
 from ._product import Product, ProductPage, get_product_page_url
 
 __all__ = [
     'BaseItem',
     'BasePage',
-    'BaseRecord',
     'Location',
     'LocationPage',
     'Product',
